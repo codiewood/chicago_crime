436e7049b6c70fb1630101fb2d6fc690b0828436
codiewood <codiejcwood@gmail.com>
2023-01-09 17:20:09 +0000

    Upload initial data processing script

---
+++ chicri/R/data_processing.R
@@ -0,0 +1,23 @@
+library(tidyr)
+library(dplyr)
+library(lubridate)
+library(forcats)
+library(stringr)
+library(readr)
+
+
+data_all <- read_csv("data/raw/Crimes_-_2019.csv")
+
+data <- data_all %>%
+  select(-c(ID, `Case Number`, Block, IUCR, `FBI Code`, Description, Beat, Ward, `Community Area`, `Updated On`, Latitude, Longitude, Location, Year))
+
+data$Date <- parse_datetime(data$Date, format = "%m/%d/%Y %I:%M:%S %p")
+
+data <- drop_na(data)
+
+data <- data %>%
+  type_convert(col_types = list(`Primary Type` = col_factor(),
+                                               `Location Description` = col_factor(),
+                                               District = col_factor()))
+
+data$`Primary Type` <- fct_collapse(data$`Primary Type`, "CRIMINAL SEXUAL ASSAULT" = c("CRIM SEXUAL ASSAULT","CRIMINAL SEXUAL ASSAULT"))
