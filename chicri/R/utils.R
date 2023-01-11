503d25153da5ed862d17dc0bba03e58cb30647c7
codiewood <codiejcwood@gmail.com>
2023-01-11 12:03:38 +0000

    Upload utils and tests

---
+++ chicri/R/utils.R
@@ -0,0 +1,20 @@
+# Script containing general purpose utility functions
+require(tidyverse)
+require(lubridate)
+
+#' Load chicago crime data from processed csv.
+#'
+#' @param filepath path to the processed csv file.
+#'
+#' @return tibble data frame of Chicago Crime data.
+#' @export
+load_crimes <- function(filepath){
+  if (grepl("raw", filepath)) {
+    stop("Please ensure the file specified contains processed data.")
+  }
+  data <- read_csv(filepath) %>%
+    type_convert(col_types = list(`Primary Type` = col_factor(),
+                                  `Location Description` = col_factor(),
+                                  District = col_factor()))
+  return(data)
+}
