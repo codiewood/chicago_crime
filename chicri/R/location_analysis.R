1903a7ad7c6e0a9a86cb2285405d91a6a6324f66
Rachel Wood <ac18826@bristol.ac.uk>
2023-01-13 16:13:40 +0000

    add location plot function

---
+++ chicri/R/location_analysis.R
@@ -0,0 +1,26 @@
+
+#' Producing a heatmap based on a variable
+#'
+#' @param data A `data.frame` containing where the shapefiles are contained in the `geometry` column
+#' @param variable A string of the column name of the variable to be used to create the heat map
+#' @param legend.title A string of the title of the legend to be included in the plot
+#' @param trans A string indicating the transformation to be applied to the scale (options are "asn", "atanh", "boxcox", "date", "exp", "hms", "identity", "log", "log10", "log1p", "log2", "logit", "modulus", "probability", "probit", "pseudo_log", "reciprocal", "reverse", "sqrt" and "time")
+#'
+#' @return A `ggplot` object with a heat map of the shapefiles in `data` with the fill colour determined by `variable`
+#'
+#' @import ggplot2 sf
+#'
+#' @export
+#'
+#' @examples
+#'
+#'
+plot_heat_map <- function(data, variable, legend.title = variable ,trans = "identity"){
+  ggplot() +
+    geom_sf(data = data, aes(fill = get(variable))) +
+    scale_fill_viridis_c(name = legend.title,option = "magma", trans = trans) +
+    theme_void()
+}
+
+
+
