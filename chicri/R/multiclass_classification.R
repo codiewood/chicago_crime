ab820403b5628591d7a443ca22b63a05b828a7eb
codiewood <codiejcwood@gmail.com>
2023-01-20 15:05:16 +0000

    Update functions and docs

--- chicri/R/multiclass_classification.R
+++ chicri/R/multiclass_classification.R
@@ -4,6 +4,9 @@
 #' @import tidyverse
 #' @import caret
 #' @importFrom stats pnorm predict
+#' @import magrittr
+#' @import forcats
+#' @import dplyr
 NULL
 
 #' Significance test for features
@@ -36,7 +39,7 @@ regroup_locations <- function(data, threshold){
   airport <- locations[grepl("AIRPORT", locations)]
   uni <- locations[grepl("UNIV", locations)]
 
-  data$`Location Description` <- fct_collapse(data$`Location Description`,
+  data$`Location Description` <- forcats::fct_collapse(data$`Location Description`,
                                               "SCHOOL" = school,
                                               "UNIVERSITY" = uni,
                                               "AIRPORT" = airport,
@@ -53,10 +56,11 @@ regroup_locations <- function(data, threshold){
 #' @param X Data to be used, not including the response variable or any variables not for use in model.
 #' @param y Response variable, as a factor.
 #' @param index The indices of the rows to be used as test data.
+#' @param return_model Boolean to indicate if the model object is returned. Default is FALSE.
 #'
-#' @return A confusion matrix, see `?confusionMatrix`
+#' @return A confusion matrix, see `?confusionMatrix`. If return_model = TRUE, a list containing a confusion matrix and a multinomial logistic regression model object, see `?multinom`
 #' @export
-mnlr_cv_indexed <- function(X, y, index){
+mnlr_cv_indexed <- function(X, y, index, return_model = FALSE){
   # Separate data into training and test data
   X_training <- X[-index, ,drop = F]
   X_testing <- X[index, ,drop=F]
@@ -72,20 +76,25 @@ mnlr_cv_indexed <- function(X, y, index){
   pred.test <- stats::predict(model, X_testing, type="class")
   # Obtain metrics
   conf.test <- caret::confusionMatrix(pred.test,y_testing,mode="everything")
-  return(conf.test)
+  if(return_model){
+    return(list(conf.matrix = conf.test, model = model))
+  }
+  else {
+    return(conf.test)
+  }
 }
 
-#' K-fold cross-validation metrics for multinomial regression.
+#' K-fold cross-validation metrics for multinomial regression, in list format.
 #'
 #' @param X Data to be used, not including the response variable or any variables not for use in model.
 #' @param y Response variable, as a factor.
 #' @param k Number of folds.
 #' @param n_reps Number of repeats.
-#' @param metrics Vector of strings with metrics to be obtained. Overall accuracy, no information rate (NIR) and the p value for accuracy > NIR will always be returned. Options are "Sensitivity", "Specificity", "Pos Pred Value", "Neg Pred Value", "Precision", "Recall", "F1", "Prevalence", "Detection Rate","Detection Prevalence" and "Balanced Accuracy".
+#' @param metrics Vector of strings with metrics to be obtained. Overall accuracy, no information rate (NIR) and the p value for accuracy > NIR will always be returned. Options are "Sensitivity", "Specificity", "Pos Pred Value", "Neg Pred Value", "Precision", "Recall", "F1", "Prevalence", "Detection Rate","Detection Prevalence" and "Balanced Accuracy". At least one must be given and must be in vector format.
 #'
 #' @return List containing two elements: a list of length equal to that of `metrics` + 3, with each element containing a list or vector of the mean of the metric at each repeat, averaged over `k` folds, and a list of length equal to `n_reps`, containing the metrics for each of the `k` folds.
 #' @export
-mnlr_kfold_cv <- function(X, y, k, n_reps = 5, metrics) {
+mnlr_kfold_cv.list <- function(X, y, k, n_reps = 5, metrics) {
   n <- nrow(X)
   m <- length(metrics)
   metric_list <- list()
@@ -152,3 +161,53 @@ mnlr_kfold_cv <- function(X, y, k, n_reps = 5, metrics) {
   return(list(mean_metrics = mean_metrics, all_metrics = metric_list))
 }
 
+#' K-fold cross-validation metrics for multinomial regression, in data frame format.
+#'
+#' @param X Data to be used, not including the response variable or any variables not for use in model.
+#' @param y Response variable, as a factor.
+#' @param k Number of folds.
+#' @param n_reps Number of repeats.
+#' @param metrics Vector of strings with metrics to be obtained. Overall accuracy, no information rate (NIR) and the p value for accuracy > NIR will always be returned. Options are "Sensitivity", "Specificity", "Pos Pred Value", "Neg Pred Value", "Precision", "Recall", "F1", "Prevalence", "Detection Rate","Detection Prevalence" and "Balanced Accuracy". At least one must be given and must be in vector format.
+#'
+#' @return List containing two elements: a data frame of the overall metrics (Overall accuracy, no information rate (NIR) and the p value for accuracy > NIR), and a data frame containing the class specific metrics given in `metrics`.
+#' @export
+mnlr_kfold_cv.df <- function(X, y, k, n_reps = 5, metrics) {
+  n <- nrow(X)
+  m <- length(metrics)
+  r <- n_reps * k
+  class_names <- levels(y)
+  c <- length(class_names)
+  # Initiate empty dfs with repeat, fold and class labels
+  df_overall <- data.frame(Repeat = as.factor(rep(1:n_reps, each=k)),
+                           Fold = as.factor(rep(1:k,n_reps)),
+                           Accuracy= rep(NA, r),
+                           NIR =rep(NA, r),
+                           Pval = rep(NA, r))
+  df_byclass <- data.frame(Repeat = as.factor(rep(1:n_reps, each=k*c)),
+                           Fold = as.factor(rep(rep(1:k,each=c),n_reps)),
+                           Class = as.factor(rep(class_names,n_reps)))
+  # Add variables for each metric
+  for(met_num in 1:m){
+    df_byclass <- df_byclass %>%
+      mutate("{metrics[met_num]}" := NA)
+  }
+  for(rep_num in 1:n_reps){
+    folds <- split(sample(1:n), 1:k)
+    for(fold_num in 1:k){
+      cm <- mnlr_cv_indexed(X, y, index = folds[[fold_num]])
+      # Create df for overall statistics
+      ind <- df_overall$Repeat == rep_num & df_overall$Fold == fold_num
+      df_overall[ind,]$Accuracy <- cm$overall[1]
+      df_overall[ind,]$NIR <- cm$overall[5]
+      df_overall[ind,]$Pval <- cm$overall[6]
+
+      # Create df for statistics by class
+      ind.c <- df_byclass$Repeat == rep_num & df_byclass$Fold == fold_num
+      for(met_num in 1:m){
+        df_byclass[ind.c,met_num+3] <- cm$byClass[,metrics][,met_num]
+      }
+
+    }
+  }
+  return(list(overall = df_overall, byclass = df_byclass))
+}
